a0950fad9dc437d4288722c8803864bfbc53225b
syan <shyness@mail.ustc.edu.cn>
2024-09-08 00:45:09 +0800

    初始化项目结构

---
+++ src/main/java/com/syan/application/MyRecipeApplication.java
@@ -0,0 +1,11 @@
+package com.syan.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRecipeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MyRecipeApplication.class, args);
+    }
+}
